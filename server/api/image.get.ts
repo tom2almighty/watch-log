e423ba2c130c02cfa7dcad28ce410e85a9aea4d7
tom2almighty <bboyc80@gmail.com>
2026-03-07 00:28:12 +0800

    fix: 为事件处理程序添加类型注解，优化代码可读性

--- server/api/image.get.ts
+++ server/api/image.get.ts
@@ -14,13 +14,15 @@ function createStatusError(statusCode: number, statusMessage: string) {
   })
 }
 
-export default markEventHandler(async (event) => {
+export default markEventHandler(async (event: any) => {
   const query = getQuery(event)
   const target = Array.isArray(query.url) ? query.url[0] : query.url
 
   if (!target) {
     throw createStatusError(400, 'Missing image url')
   }
+
+  setResponseHeader(event, 'Cache-Control', 'public, max-age=15552000')
 
   return sendProxy(event, target)
 })
